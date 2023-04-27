5710b793d1477a5ec564814162a4cf17855dbdd2
diegosales <diegofelipesales23@gmail.com>
2023-04-27 04:43:56 -0400

    estilizando o input de add task

--- src/components/InputTask/index.jsx
+++ src/components/InputTask/index.jsx
@@ -1,11 +1,14 @@
 import styles from './styles.module.scss';
+import {BiAddToQueue} from 'react-icons/bi'
 
 const InputTask = () => {
     return ( 
         <div className={styles.container}>
             <form className={styles.formContainer}>
-            <input type="text" />
-            <button>adicionar</button>
+                <input type="text" placeholder='Adicionar uma tarefa...'  />
+                <button>
+                    <BiAddToQueue />
+                </button>
             </form>
         </div>
      );
